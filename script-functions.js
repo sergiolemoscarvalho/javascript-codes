de48a4a3c5036e5070236b146b4271b73181ea67
Vora Interativa <sergio.carvalho@external.fcagroup.com>
2022-02-02 11:36:02 -0300

    destructuring - Alunos aprovados

---
+++ script-functions.js
@@ -0,0 +1,38 @@
+const alunos = [
+    {
+      nome:'João',
+      nota: 5,
+      turma: "1B",
+    },
+    {
+      nome:' Flávio',
+      nota: 6,
+      turma: '1B',
+    },
+    {
+      nome:'Henrique',
+      nota: 4,
+      turma: '1B',
+    },
+    {
+      nome: 'Vitor',
+      nota:2,
+      turma: '1B',
+    }
+   
+  ];
+  
+  function alunosAprovados(arr, media) {
+    let aprovados = [];
+    
+    for (let i=0; i < arr.length; i++){
+      const { nota, nome } = arr[i];
+      
+      if(nota >= media){
+        aprovados.push(nome);
+      }
+      
+    }
+    return aprovados;
+  }
+  console.log(alunosAprovados(alunos, 2));
